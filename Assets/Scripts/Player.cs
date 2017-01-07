49fffc3fcbb3f250aeae40c2c7937361fdb91d1f
Andrew Scott <adscott1982@googlemail.com>
2017-01-07 16:26:15 +0000

    Changed to football tactics game - implemented functionality for object to move to mouse

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 TargetPosition { get; set; }
+    public bool IsSelected { get; set; }
+    public bool IsActive { get; set; }
+    public float MaxSpeed = 2;
+
+    // Use this for initialization
+    public void Start ()
+	{
+	    this.TargetPosition = this.transform.position;
+        this.IsActive = true;
+        this.IsSelected = false;
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.TargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (this.IsActive) this.Move();
+    }
+
+    private void Move()
+    {
+        this.transform.position = Vector2.MoveTowards(this.transform.position, this.TargetPosition,
+            this.MaxSpeed * Time.deltaTime);
+    }
+}
