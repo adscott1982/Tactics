f2725960576303903891dde8bfcc5bf61a436d26
VRT\ascott <ascott@visionrt.com>
2018-09-20 19:51:40 +0100

    Added pan and zoom and some more utility methods.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera AttachedCamera;
+    private bool isPanning;
+    private Vector2 previousMousePosition;
+    private Vector2 currentMousePosition;
+    private float zoomDelta;
+
+    /// <summary>
+    /// Initialization method.
+    /// </summary>
+    private void Start ()
+    {
+        this.isPanning = false;
+	}
+	
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus)
+        {
+            this.isPanning = false;
+        }
+
+        this.previousMousePosition = this.currentMousePosition;
+    }
+
+	/// <summary>
+    /// Called every frame.
+    /// </summary>
+    private void Update ()
+	{
+        this.HandleInputs();
+        this.HandlePanning();
+        this.HandleZoom();
+	}
+
+    private void HandleZoom()
+    {
+        Camera.main.orthographicSize *= 1 + (this.zoomDelta / 10);
+    }
+
+    private void HandleInputs()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            this.isPanning = true;
+        }
+        else
+        {
+            this.isPanning = false;
+        }
+
+        this.zoomDelta = -Input.GetAxis("Mouse ScrollWheel");
+    }
+
+
+
+    private void HandlePanning()
+    {
+        this.currentMousePosition = Input.mousePosition;
+
+        Debug.Log(string.Format("Mouse X: {0}\tMouse Y: {1}", this.currentMousePosition.x, this.currentMousePosition.y));
+
+        if (!this.isPanning)
+        {
+            this.previousMousePosition = this.currentMousePosition;
+            return;
+        }
+
+        var mouseDelta = this.currentMousePosition - this.previousMousePosition;
+
+        var worldDelta = Camera.main.ScreenToWorldPoint(this.previousMousePosition) - Camera.main.ScreenToWorldPoint(this.previousMousePosition + mouseDelta);
+        Debug.Log(string.Format("Delta X: {0}\tDeltaY: {1}", mouseDelta.x, mouseDelta.y));
+        this.previousMousePosition = this.currentMousePosition;
+        
+        this.transform.position += worldDelta;
+    }
+}
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -11,6 +11,9 @@ public static class Utilities
 /// </summary>
 public static class CameraExtensions
 {
+
+    public const float FloatEqualityTolerance = 0.000001f;
+
     /// <summary>
     /// Get a random position within a given rectangle.
     /// </summary>
@@ -45,5 +48,107 @@ public static class CameraExtensions
         var y = orthoCamera.transform.position.y - (height / 2);
 
         return new Rect(x, y, width, height); ;
+    }
+
+    public static Vector3 AddVector2(this Vector3 vector3, Vector2 vector2)
+    {
+        return new Vector3(vector3.x + vector2.x, vector3.y + vector2.y);
+    }
+
+    public static float DirectionDegrees(this Vector2 vector)
+    {
+        return (Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg);
+    }
+
+    public static float RelativeDirectionDegrees(this Vector2 vector, Vector2 relativeVector)
+    {
+        // Calculate the angle of the relative vector
+        var relativeVectorAngle = relativeVector.DirectionDegrees();
+
+        // Rotate the incoming vector by the angle difference
+        var rotatedVector = vector.Rotate(-relativeVectorAngle);
+
+        // return the angle
+        return (Mathf.Atan2(rotatedVector.y, rotatedVector.x) * Mathf.Rad2Deg);
+    }
+
+    public static Vector2 Rotate(this Vector2 vector, float degrees)
+    {
+        var sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        var cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        var tx = vector.x;
+        var ty = vector.y;
+        vector.x = (cos * tx) - (sin * ty);
+        vector.y = (sin * tx) + (cos * ty);
+        return vector;
+    }
+
+    public static bool IsInRange(this float value, float firstLimit, float secondLimit)
+    {
+        if (value >= firstLimit && value <= secondLimit)
+        {
+            return true;
+        }
+
+        return value >= secondLimit && value <= firstLimit;
+    }
+
+    public static float GetLerpedRotationDelta(float currentRotation, float targetRotation, float interpolationValue, float maxRotationSpeed)
+    {
+        var lerpedRotation = Mathf.LerpAngle(currentRotation, targetRotation, interpolationValue);
+        var rotationDelta = lerpedRotation - currentRotation;
+
+        if (Mathf.Abs(rotationDelta) > maxRotationSpeed)
+        {
+            rotationDelta = Mathf.Sign(rotationDelta) * maxRotationSpeed;
+        }
+
+        return rotationDelta;
+    }
+
+    public static Quaternion AsEulerZ(this float zRotation)
+    {
+        return Quaternion.Euler(new Vector3(0, 0, zRotation));
+    }
+
+    public static Vector2 RadianToVector2(this float radian)
+    {
+        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+    }
+
+    public static Vector2 DegreeToVector2(this float degree)
+    {
+        return RadianToVector2(degree * Mathf.Deg2Rad);
+    }
+
+    public static Bounds OrthographicBounds(this Camera camera)
+    {
+        float screenAspect = (float)Screen.width / Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
+
+    public static Rect OrthographicRectInWorldSpace(this Camera camera)
+    {
+        var height = camera.orthographicSize * 2;
+        var width = height * Screen.width / Screen.height;
+        var x = camera.transform.position.x - (width / 2);
+        var y = camera.transform.position.y - (height / 2);
+
+        return new Rect(x, y, width, height);
+    }
+
+    public static Vector2 AsVector2(this Vector3 vector3)
+    {
+        return new Vector2(vector3.x, vector3.y);
+    }
+
+    public static Vector3 AsVector3(this Vector2 vector2)
+    {
+        return new Vector3(vector2.x, vector2.y, 0);
     }
 }
