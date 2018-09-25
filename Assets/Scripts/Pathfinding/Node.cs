8a3c584d6635327240abd7baf061e70622ae6167
Andrew Scott <adscott1982@googlemail.com>
2018-09-25 20:38:54 +0100

    Started adding pathfinding framework and logic.

---
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Pathfinding
+{
+    public class Node
+    {
+        public float StartCost { get; private set; }
+        public float EndCost { get; private set; }
+        public float FullCost { get; private set; }
+
+        public Vector2Int NavGridPosition { get; private set; }
+        public Vector2 Position { get; private set; }
+        public Node CameFrom { get; private set; }
+
+        public Node(Vector2Int navGridPosition, Vector2 position, Vector2 targetPosition, Node cameFrom)
+        {
+            this.NavGridPosition = navGridPosition;
+            this.Position = position;
+            this.UpdateCameFromNode(cameFrom);
+
+            // Distance from this position to target - efficient?
+            this.EndCost = Vector2.Distance(position, targetPosition);
+
+            this.FullCost = this.StartCost + this.EndCost;
+        }
+
+        public void UpdateCameFromNode(Node cameFrom)
+        {
+            this.CameFrom = cameFrom;
+
+            if (cameFrom == null)
+            {
+                this.StartCost = 0f;
+            }
+            else
+            {
+                // Start cost is current distance travelled to the previous node + new distance
+                this.StartCost = cameFrom.StartCost + Vector2.Distance(cameFrom.Position, this.Position);
+            }
+
+            this.FullCost = this.StartCost + this.EndCost;
+        }
+    }
+}
--- Assets/Scripts/Pathfinding/Pathfinder.cs
+++ Assets/Scripts/Pathfinding/Pathfinder.cs
@@ -67,7 +67,28 @@ public class Pathfinder : MonoBehaviour
     {
         this.waypoints.Clear();
 
+        var startPosition = this.transform.position;
+        var openNodes = new List<Node>();
+        var closedNodes = new List<Node>();
+
         // Step 1: A* Pathfind to target using specified nav grid size, checking each cell for collider collision with sphere, or rectangle
+
+        // Create open list - add current position
+        openNodes.Add(new Node(new Vector2Int(0, 0), startPosition, targetPosition, null));
+
+        // ** loop
+            // Select the node in openNodes with lowest full cost
+            // Remove this node from open, and add to closed
+            // If current is the target position cell, then target found, go to next step
+
+            // else, go through each neighbour of the current node, creating node or updating camefrom
+                // if neighbour with circle collider intersects grid collider or neighbour is already closed, skip to next
+
+                // if new path to neighbour is shorter or neighbour is not in openNodes
+                    // update camefrom on neighbour
+                    // if neighbour is not in open, add neighbour to open
+
+
 
         // Step 2: Raycast 2D from final waypoint to current position / first waypoint forward until finding first line of sight connection.
         // Remove intermediate waypoints
